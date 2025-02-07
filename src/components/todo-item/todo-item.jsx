1fa3b88daf931c5dbd705a6da9f75644578dbdd6
dmytro-chumak <starlexplay24@gmail.com>
2025-02-07 16:42:57 +0200

    feat(todo-item): add functionality to mark todo items as done

--- src/components/todo-item/todo-item.jsx
+++ src/components/todo-item/todo-item.jsx
@@ -1,9 +1,19 @@
 import styles from "./todo-item.module.css";
 
-export default function TodoItem({ text, done }) {
+import { useContext } from "react";
+
+import { TasksDispatchContext } from "@/context/TasksContext/TasksContext";
+
+export default function TodoItem({ id, text, done }) {
+  const dispatch = useContext(TasksDispatchContext);
+
+  function handleDone() {
+    dispatch({ type: "done", id: id });
+  }
+
   return (
     <li className={styles.item + " " + (done ? styles.done : "")}>
-      <button className={styles["button-done"]}></button>
+      <button onClick={handleDone} className={styles["button-done"]}></button>
       <span>{text}</span>
     </li>
   );
--- src/context/TasksContext/TasksReducer.js
+++ src/context/TasksContext/TasksReducer.js
@@ -1,1 +1,10 @@
-export default function tasksReducer(state, action) {}
+export default function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "done":
+      return tasks.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    default:
+      return state;
+  }
+}
