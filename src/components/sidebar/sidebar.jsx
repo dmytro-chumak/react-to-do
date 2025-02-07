1e9d46768bc56aea2f280be06daa7eb474062601
Starlex02 <starlexplay24@gmail.com>
2025-02-07 14:32:49 +0200

    feat(sidebar): add sidebar component with search and menu functionality

---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,42 @@
+import styles from "./sidebar.module.css";
+import Link from "next/link";
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <nav className={styles.sidebar}>
+        <Search />
+        <Menu />
+      </nav>
+    </aside>
+  );
+}
+
+function Search() {
+  return (
+    <div className={styles.search}>
+      <input type="text" placeholder="Search" />
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <ul className={styles.menu}>
+      <MenuItem url="/today" value="Today" />
+      <MenuItem url="/important" value="Important" />
+      <MenuItem url="/planned" value="Planned" />
+      <MenuItem url="/tasks" value="Tasks" />
+    </ul>
+  );
+}
+
+function MenuItem({ url, value }) {
+  return (
+    <li className={styles.menuItem}>
+      <Link href={url} className={styles.menuItem}>
+        {value}
+      </Link>
+    </li>
+  );
+}
