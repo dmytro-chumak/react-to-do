2ed3c568537803dd41a21f7ca974dfe228921820
Starlex02 <starlexplay24@gmail.com>
2025-02-07 14:34:08 +0200

    feat(todo-list): add TodoList component with initial todos and styling

---
+++ src/components/todo-list/todo-list.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import styles from "./todo-list.module.css";
+
+import TodoItem from "@/components/todo-item/todo-item";
+
+import { useState } from "react";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  return (
+    <div className={styles["list-container"]}>
+      <ul className={styles.list}>
+        {todos
+          .filter((todo) => !todo.done)
+          .map((todo) => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+      </ul>
+      <ul className={styles.list}>
+        {todos
+          .filter((todo) => todo.done)
+          .map((todo) => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+const initialTodos = [
+  {
+    id: 1,
+    text: "Learn React",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "Learn Next.js",
+    done: false,
+  },
+];
