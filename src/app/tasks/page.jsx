465b968d195a28fe67d41450589f870ddffbaae2
dmytro-chumak <starlexplay24@gmail.com>
2025-02-19 17:41:20 +0200

    feat(tasks): create tasks page with header, todo list, and add todo functionality

---
+++ src/app/tasks/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import styles from "./page.module.css";
+
+import Title from "@/components/title/title";
+import CurrentDay from "@/components/current-day/current-day";
+import TodoList from "@/components/todo-list/todo-list";
+import AddTodo from "@/components/add-todo/add-todo";
+import { useContext } from "react";
+import { TasksContext } from "@/context/TasksContext/TasksContext";
+
+export default function Tasks() {
+  const todos = useContext(TasksContext);
+
+  return (
+    <>
+      <div className={styles.header}>
+        <Title value="Tasks" />
+        <CurrentDay />
+      </div>
+      <TodoList todos={todos} />
+      <AddTodo />
+    </>
+  );
+}
